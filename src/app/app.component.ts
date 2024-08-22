c4dbe443f9dba717bbf0968e35840bb4f558b22b
ALFAROSO <alberth.sotelo@gmail.com>
2024-08-21 23:20:11 -0700

    hard fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,100 +1,101 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class AppComponent {
-  startDate: string = '';
-  endDate: string = '';
-  price: number = 0;
-  daysBetween: number = 0;
-
-  // Arreglo para las tasas de interes se pueden obtener de una API (https://www.ceta.org.co/html/usura.asp?txtAnod=2024&txtAnoh=2024&btnBuscar=Ver+Tasa+de+Usura)
+  totalInterestArray: number[] = [];
+  months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  currentDate = new Date();
+  currentDay = new Date().getDate();
+  currentMonth = new Date().getMonth();
+  selectedMonths = new Array(this.months.length).fill(false);
+  selectedMonthsName: string[] = [];
+  paymentDate: string;
+  value: number;
   interestRates = [
-    { month: '2024-01', rate: 34.98 },
-    { month: '2024-02', rate: 34.97 },
-    { month: '2024-03', rate: 33.30 },
-    { month: '2024-04', rate: 33.09 },
-    { month: '2024-05', rate: 31.53 },
-    { month: '2024-06', rate: 30.84 },
-    { month: '2024-07', rate: 29.49 },
-    { month: '2024-08', rate: 29.21 },
+    { month: 'Enero', rate: 34.98 },
+    { month: 'Febrero', rate: 34.97 },
+    { month: 'Marzo', rate: 33.30 },
+    { month: 'Abril', rate: 33.09 },
+    { month: 'Mayo', rate: 31.53 },
+    { month: 'Junio', rate: 30.84 },
+    { month: 'Julio', rate: 29.49 },
+    { month: 'Agosto', rate: 29.21 },
   ];
 
+
+  constructor() {
+    const today = new Date();
+    this.paymentDate = today.toISOString().split('T')[0];
+    this.value = 0;
+  }
+
+  calculateSimpleInterest(): { totalInterest: number, totalInterestArray: number[], selectedMonthsName: string[] } {
+    let totalInterestArray = [];
+    let totalInterest = 0;
+    let selectedMonthsName = [];
+    const firstTrueIndex = this.selectedMonths.findIndex(month => month === true);
+    // console.log('First True:', firstTrueIndex);
+    // let currentDate = new Date(this.startDate);
+
+    for (let i = 0; i < this.selectedMonths.length; i++) {
+      if (this.selectedMonths[i]) {
+        const month = this.months[i];
+        const interestRate = this.interestRates.find(rate => rate.month === month)?.rate ?? 0;
+        // console.log('Interest Rate:', interestRate);
+
+        let daysInMonth = 30;
+        let isLastTrue = true;
+
+        for (let j = i + 1; j < this.selectedMonths.length; j++) {
+          if (this.selectedMonths[j] === true) {
+            isLastTrue = false;
+            break;
+          }
+        }
+
+        if (this.selectedMonths[i] === true && isLastTrue) {
+          daysInMonth = this.currentDay-1;
+        }
+        let interest = (this.value * interestRate * daysInMonth) / (100 * 360);
+        totalInterest += interest * (i - firstTrueIndex + 1);
+        totalInterestArray.push(interest * (i - firstTrueIndex + 1));
+        // console.log('I:', i);
+        // totalInterestArray.push(interest); 
+        // console.log(this.months[i]);
+        // selectedMonthsName.push(this.months[i]);
+        selectedMonthsName.push(month);
+        // console.log(selectedMonthsName);
+      }
+    }
+
+    return { totalInterest, totalInterestArray, selectedMonthsName };
+  }
+
   onSubmit() {
-    const start = new Date(this.startDate);
-    const end = new Date(this.endDate);
-    const timeDiff = end.getTime() - start.getTime();
-    this.daysBetween = timeDiff / (1000 * 3600 * 24);
+    // Lógica para calcular intereses
+    const { totalInterest, totalInterestArray, selectedMonthsName } = this.calculateSimpleInterest();
+    console.log('1. Fecha actual:', this.currentDate);
+    console.log('2. current Month:', this.currentMonth);
+    console.log('2.1. Día actual:', this.currentDay);
+    console.log('2.5. Meses seleccionados TF:', this.selectedMonths);
+    console.log('3. Meses seleccionados:', selectedMonthsName);
+    console.log('4. Fecha de pago:', this.paymentDate);
+    console.log('5. Valor:', this.value);
 
-    console.log('Fecha inicio:', this.startDate);
-    console.log('Fecha fin:', this.endDate);
-    console.log('Canon:', this.price);
-    console.log('Dias en mora:', this.daysBetween);
-
-    // Calcular con interes simple
-    const simpleInterest = this.calculateSimpleInterest();
-    console.log('Total Interes a pagar (Simple):', simpleInterest);
-
-    // Calcular con interes compuesto
-    const compoundInterest = this.calculateCompoundInterest();
-    console.log('Total Interes a pagar (Compuesto):', compoundInterest);
+    // const { totalInterest, totalInterestArray, selectedMonthsName } = this.calculateSimpleInterest();
+    console.log('6. Interés simple:', totalInterest);
+    this.totalInterestArray = totalInterestArray;
+    this.selectedMonthsName = selectedMonthsName;
+    // const { totalInterest, totalInterestArray } = this.calculateSimpleInterest();
+    console.log('7. Array:', totalInterestArray)
   }
 
-  calculateSimpleInterest(): number {
-    let totalInterest = 0;
-    let currentDate = new Date(this.startDate);
-
-    while (currentDate < new Date(this.endDate)) {
-      const monthYear = `${currentDate.getFullYear()}-${('0' + (currentDate.getMonth() + 1)).slice(-2)}`;
-      const interestRate = this.interestRates.find(rate => rate.month === monthYear)?.rate ?? 0;
-
-      const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
-      const daysInCurrentMonth = Math.min(daysInMonth, (new Date(this.endDate).getDate() - currentDate.getDate() + 1));
-
-      const dailyRate = (interestRate / 100) / 365;
-
-      const interest = this.price * dailyRate * daysInCurrentMonth;
-      totalInterest += interest;
-
-      // Siguiente mes
-      currentDate.setMonth(currentDate.getMonth() + 1);
-      currentDate.setDate(1);
-    }
-
-    return totalInterest;
-  }
-
-  calculateCompoundInterest(): number {
-    let principal = this.price;
-    let currentDate = new Date(this.startDate);
-    let totalInterest = 0;
-
-    while (currentDate < new Date(this.endDate)) {
-      const monthYear = `${currentDate.getFullYear()}-${('0' + (currentDate.getMonth() + 1)).slice(-2)}`;
-      const interestRate = this.interestRates.find(rate => rate.month === monthYear)?.rate ?? 0;
-
-      const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
-      const daysInCurrentMonth = Math.min(daysInMonth, (new Date(this.endDate).getDate() - currentDate.getDate() + 1));
-
-      const dailyRate = (interestRate / 100) / 365;
-
-      const periods = daysInCurrentMonth;
-      const compoundInterestFactor = Math.pow(1 + dailyRate, periods);
-      totalInterest = principal * (compoundInterestFactor - 1);
-
-      principal += totalInterest;
-
-      // Siguiente mes
-      currentDate.setMonth(currentDate.getMonth() + 1);
-      currentDate.setDate(1);
-    }
-
-    return principal - this.price;
-  }
 }
