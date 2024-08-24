7053784a9be8ba9db3ca14d65aea9b29e83ec38f
ALFAROSO <alberth.sotelo@gmail.com>
2024-08-24 16:33:32 -0700

    substract 5 days interes on every moth rent and add total interest on table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,10 @@ import { CommonModule } from '@angular/common';
 
 export class AppComponent {
 
+  totalInterest: number = 0;
   totalInterestArray: number[] = [];
+  totalInterest5DaysArray: number[] = [];
+  resultArray: number[] = [];
   months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
   currentDate = new Date();
   currentDay = new Date().getDate();
@@ -38,8 +41,11 @@ export class AppComponent {
     this.value = 0;
   }
 
-  calculateSimpleInterest(): { totalInterest: number, totalInterestArray: number[], selectedMonthsName: string[] } {
+  //Method to calculate the simple interest
+  calculateSimpleInterest(): { totalInterest: number, totalInterestArray: number[], selectedMonthsName: string[], totalInterest5DaysArray: number[], resultArray: number[] } {
     let totalInterestArray = [];
+    let totalInterest5DaysArray = [];
+    let resultArray = [];
     let totalInterest = 0;
     let selectedMonthsName = [];
     const firstTrueIndex = this.selectedMonths.findIndex(month => month === true);
@@ -47,17 +53,16 @@ export class AppComponent {
     const dateParts: string[] = paymentDateString.split('-');
     const dayPart: string = dateParts[2];
     const lastNumber: number = parseInt(dayPart, 10);
-    // console.log('Last Number:', lastNumber);
-    
+
     for (let i = 0; i < this.selectedMonths.length; i++) {
       if (this.selectedMonths[i]) {
         const month = this.months[i];
         const interestRate = this.interestRates.find(rate => rate.month === month)?.rate ?? 0;
-        // console.log('Interest Rate:', interestRate);
 
         let daysInMonth = 30;
         let isLastTrue = true;
 
+        // calculate if is the last month
         for (let j = i + 1; j < this.selectedMonths.length; j++) {
           if (this.selectedMonths[j] === true) {
             isLastTrue = false;
@@ -65,39 +70,63 @@ export class AppComponent {
           }
         }
 
+        // if is the last month, calculate the day form date form
         if (this.selectedMonths[i] === true && isLastTrue) {
-          daysInMonth = lastNumber-1;
+          daysInMonth = lastNumber - 1;
         }
+
         let interest = (this.value * interestRate * daysInMonth) / (100 * 360);
         totalInterest += interest * (i - firstTrueIndex + 1);
         totalInterestArray.push(interest * (i - firstTrueIndex + 1));
-        // console.log('I:', i);
-        // totalInterestArray.push(interest); 
-        // console.log(this.months[i]);
-        // selectedMonthsName.push(this.months[i]);
         selectedMonthsName.push(month);
-        // console.log(selectedMonthsName);
       }
     }
 
-    return { totalInterest, totalInterestArray, selectedMonthsName };
+    // calculate 5 days interest
+    for (let i = 0; i < this.selectedMonths.length; i++) {
+      if (this.selectedMonths[i]) {
+        const month = this.months[i];
+        const interestRate = this.interestRates.find(rate => rate.month === month)?.rate ?? 0;
+        let daysInMonth = 5;
+        let interest = (this.value * interestRate * daysInMonth) / (100 * 360);
+
+        totalInterest5DaysArray.push(interest);
+      }
+    }
+
+    //subtract 5 days interest from total interest and create resultArray
+    for (let i = 0; i < totalInterestArray.length; i++) {
+      const result = totalInterestArray[i] - totalInterest5DaysArray[i];
+      resultArray.push(result);
+    }
+
+    //Subtract 5 days interest from total interest
+    const sumTotalInterest5Days = totalInterest5DaysArray.reduce((acc, curr) => acc + curr, 0);
+    totalInterest -= sumTotalInterest5Days;
+
+    return { totalInterest, totalInterestArray, selectedMonthsName, totalInterest5DaysArray, resultArray };
   }
 
   onSubmit() {
     // Lógica para calcular intereses
-    const { totalInterest, totalInterestArray, selectedMonthsName } = this.calculateSimpleInterest();
-    console.log('1. currentDate:', this.currentDate);
-    console.log('2. currentMonth:', this.currentMonth);
-    console.log('3. currentDay:', this.currentDay);
-    console.log('4. selectedMonths TF:', this.selectedMonths);
-    console.log('5. selectedMonthsName:', selectedMonthsName);
-    console.log('6. paymentDate:', this.paymentDate);
-    console.log('7. value:', this.value);
-    console.log('8. totalInterest:', totalInterest);
+    const { totalInterest, totalInterestArray, selectedMonthsName, totalInterest5DaysArray, resultArray } = this.calculateSimpleInterest();
+    this.totalInterest = totalInterest;
     this.totalInterestArray = totalInterestArray;
     this.selectedMonthsName = selectedMonthsName;
-    console.log('9. totalInterestArray:', totalInterestArray)
-    console.log('10. paymentDate:', this.paymentDate);
+    this.totalInterest5DaysArray = totalInterest5DaysArray;
+    this.resultArray = resultArray;
+    // console.log('1. currentDate:', this.currentDate);
+    // console.log('2. currentMonth:', this.currentMonth);
+    // console.log('3. currentDay:', this.currentDay);
+    // console.log('4. selectedMonths TF:', this.selectedMonths);
+    // console.log('5. selectedMonthsName:', selectedMonthsName);
+    // console.log('6. paymentDate:', this.paymentDate);
+    // console.log('7. value:', this.value);
+    // console.log('8. totalInterest:', totalInterest);
+    // console.log('9. totalInterestArray:', totalInterestArray)
+    // console.log('10. totalInterest5DaysArray:', totalInterest5DaysArray)
+    // console.log('11. paymentDate:', this.paymentDate);
+    // console.log('12. resultArray:', this.resultArray);
   }
 
 }
